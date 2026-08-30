edf3e98e1d41146213cf3df17c23c9019f97f9c4
emanuelesordo <emanuelesordo1996@gmail.com>
2026-08-30 16:23:35 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
+export const supabase = createClient(
+  'https://yyklmhzjxzkvycmxkegx.supabase.co',
+  'sb_publishable_8ZvSuQ5IJl454DLLSgbc1g_FCZxHZow'
+)
